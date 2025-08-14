4218d5c3a1b9c5822db28ce145430976e7e12aef
Muhammad Asif <muasif025@gmail.com>
2025-08-14 07:51:05 +0500

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical ChatBot",
+    version='0.0.0',
+    author="Muhammad Asif",
+    author_email="as29624041@gmmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
